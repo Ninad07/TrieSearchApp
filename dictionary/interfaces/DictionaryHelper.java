69eaf0126edc13b0d18da726fd7fc2be17885703
Ninad Deogaonkar <ninaddeo07@gmail.com>
2021-04-22 12:34:22 +0530

    Trie Search App

--- dictionary/interfaces/DictionaryHelper.java
+++
@@ -1,9 +0,0 @@
-package dictionary.interfaces;
-
-import java.util.List;
-public interface DictionaryHelper {
-    public boolean insert(String word);
-    public boolean search(String word);
-    public boolean delete(String word);
-    public List<String> autoSuggest(String word);
-}
